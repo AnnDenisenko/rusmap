054657845c191041d603ebcc60c56710d23b47e4
AnnDenisenko <anyutadeni@gmail.com>
2015-05-27 00:34:54 +0300

    Добавила функцию покраски

--- R/rusmap.R
+++ R/rusmap.R
@@ -11,3 +11,35 @@
 plotRegbyID <- function(n,layer){
   return(plot(map[map$ID_1 == n,]))
 }
+
+#' It's colour map by some parameter
+#'
+#' Colouring by parameter
+#'
+#' Map with graduate colour
+#' @param par - chosen parameter,
+#' layer_f - data frame file  
+#' @return Image of region
+#' @export
+
+colby <- function(par,layer){
+  ggplot(layer_f, aes(long, lat, group = group, fill = par)) +
+    geom_polygon() +
+    coord_equal() +
+    # For white lines
+    ggtitle("MAP by par") + geom_polygon(data = layer_f, aes(long,lat), 
+                                    fill=NA, 
+                                    color = "darkblue",
+                                    size=0.1)+ 
+    
+    # If we don't want text
+    theme(axis.line=element_blank(),   
+          axis.text.x=element_blank(), 
+          axis.text.y=element_blank(),
+          axis.ticks=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          legend.position="none",
+          panel.background=element_blank())
+   + scale_fill_gradient(low = "lightblue", high = "blue")
+}
